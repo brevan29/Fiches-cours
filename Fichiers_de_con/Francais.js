26c23c1d2809404caedf1021b0f1d425f365127c
Brévan Hamel <brevanhamel29150@gmail.com>
2025-10-31 16:02:19 +0100

    Banger

--- Fichiers_de_con/Francais.js
+++ Fichiers_de_con/Francais.js
@@ -3,9 +3,15 @@ Raccourcis.set("GC", "Georges Canguilhem");
 Raccourcis.set("D", "Descartes");
 Raccourcis.set("CB", "Claude Bernard");
 
+var AntiRaccourcis = new Map();
+AntiRaccourcis.set("Georges Canguilhem","GC");
+AntiRaccourcis.set("Descartes","D");
+AntiRaccourcis.set("Claude Bernard","CB");
+
 function ChangerRaccourci(AC){
     let EltsAC = document.getElementsByClassName(AC)
     for (i=0; i<EltsAC.length; i++){
-        EltsAC[i].innerHTML = Raccourcis.get(AC)
-    }
+        if (EltsAC[i].innerHTML.length<=2){EltsAC[i].innerHTML = Raccourcis.get(AC)}
+        else {EltsAC[i].innerHTML = AntiRaccourcis.get(EltsAC[i].innerHTML)}
+        }
 }
